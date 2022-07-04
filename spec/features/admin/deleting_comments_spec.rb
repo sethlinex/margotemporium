62d8cf7a7e0a0055c6a89f24eb78bf72a7a4004f
concreteguillotine <sethlinex@gmail.com>
2022-07-04 09:41:44 -0400

    added ability to delete comments for admins

--- spec/features/admin/deleting_comments_spec.rb
+++ spec/features/admin/deleting_comments_spec.rb
@@ -1,15 +1,13 @@
 require "rails_helper"
 
 RSpec.feature "Admins can remove comments from Margot posts" do
-  let(:user) { FactoryBot.create(:user, :admin) }
+  let(:admin) { FactoryBot.create(:user, :admin) }
+  let(:user) { FactoryBot.create(:user) }
   let!(:post) { FactoryBot.create(:post) }
   let(:comment) { FactoryBot.create(:comment, post: post) }
 
-  before do
-    login_as(user)
-  end
-
   scenario "by clicking a link" do
+    login_as(admin)
     visit post_path(post)
 
     within(".comments") do
@@ -19,5 +17,18 @@ RSpec.feature "Admins can remove comments from Margot posts" do
     end
 
     expect(page).to have_content "Comment removed successfully."
+  end
+
+  scenario "but regular users can't" do
+    login_as(user)
+    visit post_path(post)
+
+    within(".comments") do
+      fill_in "Text", with: "Added a comment!"
+      click_button "Create Comment"
+
+      expect(page).to have_content "Added a comment!"
+      expect(page).to_not have_button "Delete Comment"
+    end
   end
 end
