861333a955be8685877e11fc63baf950f99a6a9f
lovablescamp <lovablescamp@outlook.com>
2022-07-04 13:56:30 +1000

    Disable comment request spec

--- spec/requests/admin/comments_request_spec.rb
+++ spec/requests/admin/comments_request_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::Comments", type: :request do
-
-  describe "GET /delete" do
+  xdescribe "GET /delete" do
     it "returns http success" do
       get "/admin/comments/delete"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
